e4c17bb385c027ec7dbab6a04228e6a71166d53d
freakyvicky <freakyvicky89@gmail.com>
2020-05-04 20:45:36 +0200

    MongoDB persistence

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
+import credentials
+
 from lxml import html
 from lxml.etree import XPath
+from pymongo import MongoClient
 
 gotolast_xpath = XPath("//a[@class='page-link bg-light' and contains(.,'»»')]")
 author_xpath = XPath("//h4[@class='smBox']/a")
@@ -10,6 +13,11 @@ authors_url = "/autorzy/?pspn=%s"
 authors_url_paging = "?ppn=%s"
 
 authors_index = 1
+
+db_client = MongoClient('mongodb://%s:%s@192.168.0.214/cytaty' % (credentials.user, credentials.passwd))
+db = db_client.get_database('cytaty')
+dao = db['autorzy']
+dao.delete_many({})
 
 while True:
     authors_page = html.parse(base_url + authors_url % authors_index)
@@ -33,7 +41,17 @@ while True:
 
         print(author_quotes)
 
+        new_author = {
+            'nazwa' : author_name,
+            'cytaty' : author_quotes
+        }
+
+        print('Stored: %s' % dao.insert_one(new_author).inserted_id)
+        print('___________________________________________________')
+
     if not gotolast_xpath(authors_page):
         break
     else:
         authors_index += 1
+
+print('Saved authors: %s' % dao.count({}))
